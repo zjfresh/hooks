0deabf772729299c629972569ec79563aedade50
zj21 <zj21@meitu.com>
2022-09-06 16:01:43 +0800

    添加 useUpdate 在 unmounted 时的容错处理 (#1866)

--- packages/hooks/src/useUpdate/index.ts
+++ packages/hooks/src/useUpdate/index.ts
@@ -1,9 +1,15 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const useUpdate = () => {
   const [, setState] = useState({});
+  const mountedRef = useRef(false);
+  useEffect(() => {
+    mountedRef.current = true;
+  });
 
-  return useCallback(() => setState({}), []);
+  return useCallback(() => {
+    if (mountedRef.current) setState({});
+  }, []);
 };
 
 export default useUpdate;
